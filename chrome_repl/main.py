7db0d3ca2c4ec2b6d7884f6dc83ee67642857f3e
alexey <leksito1994@gmail.com>
2018-11-14 03:02:09 +0300

    project structure

---
+++ chrome_repl/main.py
@@ -0,0 +1,16 @@
+import PyChromeDevTools
+
+class REPL:
+    def __init__(self):
+        """TODO: Docstring for __init__.
+        :returns: TODO
+
+        """
+        pass
+
+    def run(self):
+        while True:
+            pass
+
+if __name__ = '__main__':
+    pass
