8894d4cd26de28a6a160edc6b753fef5824bc9f7
leksito <leksito1994@gmail.com>
2019-10-07 01:57:52 +0300

    added selectable queue to utils

--- chrepl/common/util.py
+++ chrepl/common/util.py
@@ -1,4 +1,10 @@
 import threading
+try
+    import Queue as queue
+except ImportError:
+    import queue
+import os
+
 
 class _Singleton(type):
     """ A metaclass that creates a Singleton base class when called. """
@@ -9,6 +15,26 @@ class _Singleton(type):
         return cls._instances[cls]
 
 class Singleton(_Singleton('SingletonMeta', (object,), {})): pass
+
+
+class SelectableQueue(queue.Queue):
+    def __init__(self):
+        super().__init__()
+        self._putsocket, self._getsocket = socket.socketpair()
+
+    def fileno(self):
+        return self._getsocket.fileno()
+
+    def put(self, item):
+        super().put(item)
+        self._putsocket.send(b'x')
+
+    def get(self):
+        self._getsocket.recv(1)
+        return super().get()
+
+    def is_not_empty(self):
+        return not super().empty()
 
 
 print_mutex = threading.Lock()
